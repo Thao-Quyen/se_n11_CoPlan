455a48b5d0ff9e82bd97677f9ab37bc82b57008c
Linh <thuylinhngo773@gmail.com>
2023-05-19 14:54:51 +0700

    l's

---
+++ code/Pages/Screen/HomeScreen/Must_travel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MustTravelCard extends StatelessWidget {
+  final String placeName;
+  final String ImagePath;
+  const MustTravelCard(
+      {super.key, required this.placeName, required this.ImagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect();
+  }
+}
